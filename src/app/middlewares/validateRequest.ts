aab2b4c99a5bef4ac8ce1a9fe201c603787ce057
Arafat Rahman <arafatrahman862@gmail.com>
2025-07-25 00:06:43 +0600

    single and multiple pic upload added

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -3,10 +3,14 @@ import { AnyZodObject } from "zod";
 export const validateRequest =
   (zodSchema: AnyZodObject) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      req.body = await zodSchema.parseAsync(req.body);
-      next();
-    } catch (error) {
-      next(error);
+  try {
+    // req.body =JSON.parse(req.body.data || {}) || req.body
+    if (req.body.data) {
+      req.body = JSON.parse(req.body.data);
     }
+    req.body = await zodSchema.parseAsync(req.body);
+    next();
+  } catch (error) {
+    next(error);
+  }
   };
--- src/app/modules/division/division.route.ts
+++ src/app/modules/division/division.route.ts
@@ -3,14 +3,24 @@ import { DivisionController } from "./division.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { createDivisionSchema, updateDivisionSchema } from "./division.validation";
+import {  createDivisionSchema, updateDivisionSchema } from "./division.validation";
+import { multerUpload } from "../../config/multer.config";
 
 
 const router = Router();
+/*
+ {
+
+ file : Image
+ data : body text data => req.body => req.body.data
+ }
+*/
+// Form data -> body, file
 
 router.post(
   "/create",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  multerUpload.single("file"),
   validateRequest(createDivisionSchema),
   DivisionController.createDivision
 );
